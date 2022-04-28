c04af9a4fae3319c0e595a294201eb029fc5d590
Lucas Weinberg <Lucas.Weinberg@uibk.com>
2022-04-28 17:53:08 +0200

    Stephansdom Sehenswürdigkeit hinzugefügt

--- main.js
+++ main.js
@@ -19,5 +19,24 @@ let map = L.map("map", {
 
 let layerControl = L.control.layers ({
 "BasemapAT Grau": startLayer,
+"Basemap Standard": L.tileLayer.provider ("BasemapAT.basemap"),
+"Basemap High-DPI": L.tileLayer.provider ("BasemapAT.highdpi"),
+"Basemap Gelände":L.tileLayer.provider ("BasemapAT.terrain"),
+"Basemap Oberfläche":L.tileLayer.provider ("BasemapAT.surface"),
+"Basemap Orthofoto": L.tileLayer.provider ("BasemapAT.orthofoto"),
+"Basemap Beschriftung": L.tileLayer.provider ("BasemapAT.overlay"),
+"Basemap mit Orthofoto und Beschriftung": L.layerGroup ([
+    L.tileLayer.provider ("BasemapAT.orthofoto"),
+    L.tileLayer.provider ("BasemapAT.overlay"),
 
+ ]
+
+)
 }). addTo(map);
+layerControl.expand();
+
+let sightLayer = L.featureGroup();
+
+layerControl.addOverlay(sightLayer, "Sehenswürdigkeit");
+
+let mrk = L.marker ([stephansdom.lat, stephansdom.lng]). addTo(sightLayer);
