39bfac38eb8ee152c621992738a582d37eef6cc7
Lucas Weinberg <Lucas.Weinberg@uibk.com>
2022-04-28 17:31:38 +0200

    Karte mit Startlayer

--- main.js
+++ main.js
@@ -6,3 +6,18 @@ let stephansdom = {
   lng: 16.373118,
   title: "Stephansdom"
 };
+
+let startLayer = L.tileLayer.provider("BasemapAT.grau");
+
+let map = L.map("map", {
+    center: [stephansdom.lat, stephansdom.lng],
+    zoom: 12,
+    layers: [
+        startLayer
+    ],
+});
+
+let layerControl = L.control.layers ({
+"BasemapAT Grau": startLayer,
+
+}). addTo(map);
